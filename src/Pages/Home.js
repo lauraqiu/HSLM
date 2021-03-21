eeffb4aa235674798edecca37d3a51742ee84d64
lauraqiu <lauraqiu21@gmail.com>
2021-03-20 23:08:37 -0400

    add home page

---
+++ src/Pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import NavBar from '../Components/NavBar.js'
+
+const HomePage = () => {
+
+    const NavOptions = [
+        {label: 'Home'},
+        {label: 'Quizzes'},
+        {label: 'Choose Your Own Adventure'},
+        {label: 'Videos'},
+        {label: 'Contact Us!'}
+        ]
+
+    return (
+        <div> 
+            <NavBar options={NavOptions}/>
+        </div>
+    )
+}
+export default HomePage;
