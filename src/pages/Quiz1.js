b79737eade288278ade1d9d0b0157e07bfc47534
Cindy726 <76708484+Cindy726@users.noreply.github.com>
2021-08-25 11:26:10 -0400

    Revert "reverted commits"
    This reverts commit c4108d981cb0c2f1fc18bb5f04f28ade7ddfd827.

--- src/pages/Quiz1.js
+++ src/pages/Quiz1.js
@@ -24,6 +24,10 @@ import Quiz1Q5O1 from "../images/Quiz1/17.png";
 import Quiz1Q5O2 from "../images/Quiz1/18.png";
 import Quiz1Q5O3 from "../images/Quiz1/19.png";
 import Quiz1Q5O4 from "../images/Quiz1/20.jpg";
+import Quiz1A1 from "../images/Quiz1/Answer1.jpg";
+import Quiz1A2 from "../images/Quiz1/Answer2.jpg";
+import Quiz1A3 from "../images/Quiz1/Answer3.jpg";
+import Quiz1A4 from "../images/Quiz1/Answer4.jpg";
 const Quiz = styled(ReactBuzzFeedQuiz)([]);
 
 const Quizzes = () => {
@@ -34,9 +38,9 @@ const Quizzes = () => {
       <NavBar />
       <Grid container className={classes.page}>
         <Quiz
-          title={"Your title goes here."} // quiz title name needed
+          title={"What type of pet should you get?"} 
           description={
-            "Choose a picture in every colour and we’ll tell you a name!"
+            "Choose a picture in every colour and we’ll tell you a type!"
           }
           byline={false}
           autoScroll={true}
@@ -72,6 +76,8 @@ const Quizzes = () => {
             },
             {
               question: "White",
+              backgroundColor: "White",
+              fontColor: "Black",
               questionRelativeToImage: "overlap",
               answerArrangement: "tile",
               answers: [
@@ -95,6 +101,7 @@ const Quizzes = () => {
             },
             {
               question: "Green",
+              backgroundColor: "#2BD535",
               questionRelativeToImage: "overlap",
               answerArrangement: "tile",
               answers: [
@@ -118,6 +125,7 @@ const Quizzes = () => {
             },
             {
               question: "Pink",
+              backgroundColor: "Pink",
               questionRelativeToImage: "overlap",
               answerArrangement: "tile",
               answers: [
@@ -141,6 +149,7 @@ const Quizzes = () => {
             },
             {
               question: "Blue",
+              backgroundColor: "#3CC8FF",
               questionRelativeToImage: "overlap",
               answerArrangement: "tile",
               answers: [
@@ -165,29 +174,32 @@ const Quizzes = () => {
           ]}
           results={[
             {
-              title: "Your first result title goes here",
+              title: "Cat",
               description:
                 "You like to go with the flow. Cats love to keep to a routine.",
-              imageAttribution: "Your photo attribution text goes here",
+              resultImageSrc: Quiz1A1,
               resultID: 0,
             },
             {
-              title: "Your second result title goes here",
+              title: "Dog",
               description:
                 "You’re very adventurous. What better way to explore than with a furry companion like a dog?",
-              resultID: 1,
+                resultImageSrc: Quiz1A2,
+                resultID: 1,
             },
             {
-              title: "Your third result title goes here",
+              title: "Rabbit",
               description:
                 "You’re free spirited. What better way than with a cute rabbit by your side!",
-              resultID: 2,
+                resultImageSrc: Quiz1A3,
+                resultID: 2,
             },
             {
-              title: "Your third result title goes here",
+              title: "Guinea Pig",
               description:
                 "You’re very down to earth! You like to keep things simple and what better way to do so then with a nice furry animal like a mouse of a guinea pig?",
-              resultID: 3,
+                resultImageSrc: Quiz1A4,
+                resultID: 3,
             },
           ]}
         />
