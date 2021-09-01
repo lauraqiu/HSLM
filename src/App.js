e6dfd3deb23aae66902726cfaf1a2e8db861b717
lauraqiu <46204084+lauraqiu@users.noreply.github.com>
2021-08-31 22:02:54 -0400

    deleted adventure pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Quiz1 from './pages/Quiz1.js';
 import Quiz2 from './pages/Quiz2.js';
 import Quiz3 from './pages/Quiz3.js';
 import Quiz4 from './pages/Quiz4.js';
-import AdventurePage from './pages/Adventure.js';
 import ContactForm from './pages/ContactForm.js';
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import "@fontsource/poppins";
@@ -18,7 +17,6 @@ function App() {
         <Route path='/quiz2' component={Quiz2}/>
         <Route path='/quiz3' component={Quiz3}/>
         <Route path='/quiz4' component={Quiz4}/>
-        <Route path='/adventure' component={AdventurePage}/>
         <Route path='/contactform' component={ContactForm}/>
         <Route path='/' component={HomePage}/>
       </Switch>
